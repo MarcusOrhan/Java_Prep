01968c541f60697afc9c11e2d046a253e6aa3dec
marcusorhan <marcusorhan@gmail.com>
2022-12-02 21:33:23 -0500

    second prime number method

--- src/SampleSolutionsMethods/PrimeNumber.java
+++ src/SampleSolutionsMethods/PrimeNumber.java
@@ -2,7 +2,9 @@ package SampleSolutionsMethods;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        checkPrime(6);
+
+      //  checkPrime(6);
+        isPrime(22);
     }
     public static boolean checkPrime(int n) {
         if (n <= 1) {
@@ -16,4 +18,27 @@ public class PrimeNumber {
         }
         System.out.println(n +" is a prime number ");
         return true; }
+
+    public static void isPrime(int number) {
+        int i,m=0,flag=0;
+        int n=number;//it is the number to be checked
+        m=n/2;
+        if(n==0||n==1){
+            System.out.println(n+" is not prime number");
+        }else{
+            for(i=2;i<=m;i++){
+                if(n%i==0){
+                    System.out.println(n+" is not prime number");
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==0)  { System.out.println(n+" is prime number");
+            }
+        }//end of else
+    }
 }
+
+
+
+
