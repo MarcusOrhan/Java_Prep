357123fdcc439d63fcbdbc08dccfbd0c3672e5dd
marcusorhan <marcusorhan@gmail.com>
2022-12-08 21:27:02 -0500

    again Bubble Sort

--- src/SampleSolutionsMethods/Sort.java
+++ src/SampleSolutionsMethods/Sort.java
@@ -1,4 +1,22 @@
 package SampleSolutionsMethods;
 
 public class Sort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    temp = arr[j-1]; //swap elements
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                } } } }
+    public static void main(String[] args) {
+        int arr[] = {3, 60, 35, 2, 45, 320, 5};
+        bubbleSort(arr);//sorting array elements using bubble sort
+        System.out.println("Array After Bubble Sort");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
