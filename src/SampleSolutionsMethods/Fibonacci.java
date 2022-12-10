3f0f04c8bfdc1ae059c63ed1ded99b19ec8179a0
marcusorhan <marcusorhan@gmail.com>
2022-12-10 00:42:47 -0500

    fibonacci simlest solution added

--- src/SampleSolutionsMethods/Fibonacci.java
+++ src/SampleSolutionsMethods/Fibonacci.java
@@ -1,4 +1,17 @@
 package SampleSolutionsMethods;
 
 public class Fibonacci {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " " + b + " ");
+        for(int i = 2; i <=10; i++) {
+            int c = a+b;
+            a = b;
+            b = c;
+            System.out.print(c + " + ");
+        }
+    }
+
 }
+
