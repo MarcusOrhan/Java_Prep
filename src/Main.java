04627bb4a2cf06129f1d37792aae6601b6586d1a
Marcus Orhan <97305055+MarcusOrhan@users.noreply.github.com>
2022-12-27 20:08:19 -0500

    MarArray (#30)
    * arrays
    
    * from master (#29)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
+        int sayi1 = 42;
+        int sayi2 = 5;
+        int sayi3 = 123;
 
+int enbuyuk=sayi1;
+if (sayi1<enbuyuk){
+    enbuyuk=sayi2;
+} if (sayi2<enbuyuk) {
+    enbuyuk=sayi3;
+}
 
+        System.out.println("enbuyuk = " + enbuyuk);
     }
 }
