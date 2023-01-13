221b0730d4217a1b2dadfa765d55edef1e468a3e
glnsb <marcusorhan@gmail.com>
2023-01-13 00:52:29 -0500

    from glnsb Mar Customer manager methods added

--- src/value_and_reference_types/CustomerManager.java
+++ src/value_and_reference_types/CustomerManager.java
@@ -1,24 +1,17 @@
 package value_and_reference_types;
 
 public class CustomerManager {
-    public static void main(String[] args) {
-        CustomerManager customerManager = new CustomerManager();
-        CustomerManager customerManager2 = new CustomerManager();
-        customerManager = customerManager2;
-//        customerManager.Add();
-//        customerManager.Remove();
-//        customerManager.Update();
 
-//Primitive data types - Value
-        int sayi1 = 10;
-        int sayi2 = 20;
-        sayi2 = sayi1;
-        sayi1 =30;
-        System.out.println("sayi2 = " + sayi2);// sayi2=10
+  public void Add(){
+      System.out.println(" One new Customer successfully added");
+  }
 
-
-        int[] sayilar1= new int[]{1,2,3,};
-
+  public void Remove(){
+      System.out.println("The most recent Customer successfully removed");
+  }
+    public void Update(){
+        System.out.println("The Customer successfully updated");
     }
+
 
 }
