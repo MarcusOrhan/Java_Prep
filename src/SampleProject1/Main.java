1818f029b466563494b60664fff9aa310c5e2aad
glnsb <marcusorhan@gmail.com>
2023-01-14 03:50:21 -0500

    demo rour operations

---
+++ src/SampleProject1/Main.java
@@ -0,0 +1,10 @@
+package SampleProject1;
+
+public class Main {
+    public static void main(String[] args) {
+        FourOperations fourOperations = new FourOperations();
+        int result = fourOperations.add(11,13);
+        System.out.println("result = " + result);
+
+    }
+}
