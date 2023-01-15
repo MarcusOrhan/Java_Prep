6e343a99bf0521a3e4b0cf09ec13e1015198b147
glnsb <marcusorhan@gmail.com>
2023-01-15 03:05:38 -0500

    from Mar: Product and ProductManager  encapsulation , costructors (with and without parameters)  added

--- src/claseswithAttributes/Main.java
+++ src/claseswithAttributes/Main.java
@@ -1,15 +1,21 @@
 package claseswithAttributes;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Product product = new Product();
-        product.name = "Laptop";
+        Product product = new Product(002,"ChromeBook","Lenovo", 799, 7,"Gray");
+  /*
+        product.setName("Laptop");
         product.id = 001;
-        product.description = "new Asus Laptop in its unopened box";
-        product.price = 420.99;
-        product.stockAmount = 13;
-
-        System.out.println("product Name= " +product.name);
+        product.setDescription("new Asus Laptop in its unopened box");
+        product.setPrice(420.99);
+        product.setStockAmount(13);
+*/
+        System.out.println("product Name= " + product.getName());
+        ProductManager productManager = new ProductManager();
+        productManager.Add(product);
+        System.out.println("product code is: "+ product.getCode());
 
     }
 }
--- src/claseswithAttributes/ProductManager.java
+++ src/claseswithAttributes/ProductManager.java
@@ -2,7 +2,8 @@ package claseswithAttributes;
 
 public class ProductManager {
     public void Add(Product product){
-
+//JDBC
+        System.out.println("product = " + product.getName() + " added by  Add method");
 
     }
 }
