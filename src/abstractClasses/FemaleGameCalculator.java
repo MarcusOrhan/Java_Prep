b42744663b16b2799ddd3af1a26ee72845804c19
glnsb <marcusorhan@gmail.com>
2023-01-17 21:32:24 -0500

    from Mar:overriding and abstract classes

---
+++ src/abstractClasses/FemaleGameCalculator.java
@@ -0,0 +1,4 @@
+package abstractClasses;
+
+public class FemaleGameCalculator extends GameCalcculator{
+}
---
+++ src/abstractClasses/GameCalcculator.java
@@ -0,0 +1,7 @@
+package abstractClasses;
+
+public class GameCalcculator {
+    public void hesapla(){
+        System.out.println(" Puaniniz = 100 " );
+    }
+}
