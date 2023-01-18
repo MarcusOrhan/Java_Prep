150dfc4b77ae7d711a4a8a27c8d3deb92bbecc2a
glnsb <marcusorhan@gmail.com>
2023-01-17 23:16:26 -0500

    from Mar: Interfaces

---
+++ src/interfaces/MySqlCustomerDal.java
@@ -0,0 +1,8 @@
+package interfaces;
+//multiple interfaces may be implemented by a class whereas only one abstract is extended by a class.
+public class MySqlCustomerDal implements ICustomerDal, IRepository{
+    @Override
+    public void add() {
+        System.out.println(" MySQL DAl ADDED");
+    }
+}
---
+++ src/interfaces/main.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public class main {
+    public static void main(String[] args) {
+        ICustomerDal customerDal = new MySqlCustomerDal();
+
+
+    }
+}
