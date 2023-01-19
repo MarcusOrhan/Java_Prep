617579c7c1313e0358f0f7ba45d2f1f27ccad81b
glnsb <marcusorhan@gmail.com>
2023-01-18 23:34:25 -0500

    polymorphism in interface

--- src/interfaces/main.java
+++ src/interfaces/main.java
@@ -2,9 +2,10 @@ package interfaces;
 
 public class main {
     public static void main(String[] args) {
-    //    ICustomerDal customerDal = new MySqlCustomerDal();
-CusomerManager cusomerManager = new CusomerManager();
-cusomerManager.customerDal = new OracleCustomerDal();
-cusomerManager.add();
+        // ICustomerDal customerDal = new MySqlCustomerDal();
+        // customerManager.customerDal = new OracleCustomerDal();
+CustomerManager customerManager = new CustomerManager(new MySqlCustomerDal());
+
+customerManager.add();
     }
 }
