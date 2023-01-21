5cba00634cf6d3863a64d4cd823091834152c8a9
glnsb <marcusorhan@gmail.com>
2023-01-21 01:06:06 -0500

    Diff Array Solution started for codewarws

--- src/arrays/Arrays1.java
+++ src/arrays/Arrays1.java
@@ -3,6 +3,8 @@ package arrays;
 public class Arrays1 {
     public static void main(String[] args) {
         double[] myList = {1.2, 1.3, 7.3, 9.3};
+
+
 double total=0;
 double max = myList[0];
 for(double number:myList){
--- src/arrays/MultiDimensionArray.java
+++ src/arrays/MultiDimensionArray.java
@@ -4,7 +4,6 @@ public class MultiDimensionArray {
 
     public static void main(String[] args) {
         String[][] sehirler = new String[3][3];
-
         sehirler[0][0] = "Istanbul";
         sehirler[0][1] = "Bursa";
         sehirler[0][2] = "Canakkale";
@@ -14,6 +13,7 @@ public class MultiDimensionArray {
         sehirler[2][0] = "Adana";
         sehirler[2][1] = "Hatay";
         sehirler[2][2] = "Oamaniye";
+
         for (int i = 0; i <= sehirler.length - 1; i++) {
             System.out.println("*******======================******");
             for (int j = 0; j <= 2; j++) {
