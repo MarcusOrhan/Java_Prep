f38907add23190840fe7223bd57df4feec60e15d
glnsb <marcusorhan@gmail.com>
2023-01-22 01:30:54 -0500

    Hash MAp

---
+++ src/hash_map_demo/Main.java
@@ -0,0 +1,21 @@
+package hash_map_demo;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String,String> sozluk= new HashMap<String, String>();
+        sozluk.put("Book", "Kitap");
+        sozluk.put("Booklet", "Kitapçık");
+        sozluk.put("Book Shelf", "Kitaplık");
+        sozluk.put("Book store", "Kitapçı");
+        System.out.println(sozluk.size());
+        for (String item: sozluk.keySet()
+             ) {
+            System.out.println("item = " + item+ ", Value = "+sozluk.get(item));
+        }
+        sozluk.remove("Book");
+        System.out.println("sozluk = " + sozluk.get("Booklet"));
+        System.out.println( sozluk.get("Book"));//null
+    }
+}
