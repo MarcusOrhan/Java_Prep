f509c9df5c4c039ed7a06e0f18f3cd44c8e6c6a2
glnsb <marcusorhan@gmail.com>
2023-01-25 00:07:33 -0500

    file creation and reading createFile() Method

--- src/file_reading/Main.java
+++ src/file_reading/Main.java
@@ -5,13 +5,18 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+
+
+    }
+
+    public static void createFile(){
         File file = new File("C:\\Users\\glnsb\\Desktop\\mesut\\students.txt");
         try {
-           if (file.createNewFile()) {
-               System.out.println("Dosya olusturuldu");
-           }else{
-               System.out.println("Dosya olusturulamadi Maalesf yaada dosya zaten mevcut");
-           }
+            if (file.createNewFile()) {
+                System.out.println("Dosya olusturuldu");
+            }else{
+                System.out.println("Dosya olusturulamadi Maalesf yaada dosya zaten mevcut");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
