3fab89328cb5b5064e0adac35c1307168f2cbcce
glnsb <marcusorhan@gmail.com>
2023-01-24 23:40:22 -0500

    cusom throw demo

--- src/throw_demo/AccountManager.java
+++ src/throw_demo/AccountManager.java
@@ -2,6 +2,19 @@ package throw_demo;
 
 public class AccountManager {
     private double balance;
+public void deposit(double amount){
+    balance = getBalance() + amount;
+}
+public void withdraw(double amount) throws Exception {
+    if (balance>= amount){
+        balance = getBalance() - amount;
+    }else {
+       throw new Exception( "Hesaptan para cekilemedi, Yetersiz bakiye Exception");
+    }
 
+}
 
+    public double getBalance() {
+        return balance;
+    }
 }
