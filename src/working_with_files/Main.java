62177cb77346cc91c903fcdce6093629543e4b15
glnsb <marcusorhan@gmail.com>
2023-01-26 01:54:21 -0500

    file writing

--- src/working_with_files/Main.java
+++ src/working_with_files/Main.java
@@ -1,23 +1,20 @@
-package file_reading;
+package working_with_files;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-//getFileInfo();
-readFile();
+        writeFile();
+        readFile();
+        //getFileInfo();
     }
-
-    public static void createFile(){
+    public static void createFile() {
         File file = new File("C:\\Users\\glnsb\\Desktop\\mesut\\students.txt");
         try {
             if (file.createNewFile()) {
                 System.out.println("Dosya olusturuldu");
-            }else{
+            } else {
                 System.out.println("Dosya olusturulamadi Maalesf yaada dosya zaten mevcut");
             }
         } catch (IOException e) {
@@ -25,9 +22,9 @@ readFile();
         }
     }
 
-    public static void getFileInfo(){
+    public static void getFileInfo() {
         File file = new File("C:\\Users\\glnsb\\Desktop\\mesut\\students.txt");
-        if(file.exists()){
+        if (file.exists()) {
             System.out.println("file name- dosya adi: " + file.getName());
             System.out.println("file path - dosya yolu: " + file.getAbsolutePath());
             System.out.println("can file readable - dosya okunabilir : " + file.canRead());
@@ -36,21 +33,43 @@ readFile();
         }
     }
 
-    public static void readFile(){
+    public static void readFile() {
         File file = new File("C:\\Users\\glnsb\\Desktop\\mesut\\students.txt");
 
         try {
             Scanner reader = new Scanner(file);
-            while (reader.hasNextLine()){
+            while (reader.hasNextLine()) {
                 String line = reader.nextLine();
                 System.out.println("line = " + line);
             }
             reader.close();
         } catch (FileNotFoundException e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
-
-
     }
 
+    public static void writeFile() {
+        try {
+
+            //  FileWriter fileWriter = new FileWriter("C:\\Users\\glnsb\\Desktop\\mesut\\students.txt");
+            BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\glnsb\\Desktop\\mesut\\students.txt"));
+            writer.newLine();
+            writer.write("Muhammad Ali Clay \uD83D\uDE0E");
+            writer.newLine();
+            writer.write("Marcus \uD85D\uDE5E");
+            writer.newLine();
+            writer.write("Jasmine \uD85D\uDE6E");
+            writer.newLine();
+            writer.write("Betsy \uD85D\uDE7E");
+            writer.newLine();
+            writer.write("Isabell \uD85D\uDE8E");
+            writer.newLine();
+            writer.write("Elizabeth \uD85D\uDE9E");
+            System.out.println(" student Dosyasina ;; eklendi");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
