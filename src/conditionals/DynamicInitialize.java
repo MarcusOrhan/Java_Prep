2741011661d11d9b378b374a53ad75f01e230ca5
glnsb <marcusorhan@gmail.com>
2023-02-04 03:34:40 -0500

    increment

--- src/conditionals/DynamicInitialize.java
+++ src/conditionals/DynamicInitialize.java
@@ -1,4 +1,11 @@
 package conditionals;
 
 public class DynamicInitialize {
+    public static void main(String[] args) {
+        double a, b; a = 3.0; b = 4.0;
+
+        double c = Math.sqrt(a * a + b * b);
+
+        System.out.println(c);
+    }
 }
