ce0fe5fe7c8d07c79924aa5f940faca50e876495
glnsb <marcusorhan@gmail.com>
2023-02-04 00:14:44 -0500

    increment

---
+++ src/increment.java
@@ -0,0 +1,15 @@
+public class increment {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+
+        if (x++<++y){
+
+            System.out.println("Hello");
+        }else{
+            System.out.println("Welcome");
+        }
+        System.out.println("Log "+x+" : "+ y);
+    }
+
+}
