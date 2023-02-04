c47899a3d5e03bd5d142c3bad0c9b65f3e1d40d2
glnsb <marcusorhan@gmail.com>
2023-02-04 03:28:39 -0500

    increment

--- src/arrays/Array.java
+++ src/arrays/Array.java
@@ -2,16 +2,11 @@ package arrays;
 
 public class Array {
     public static void main(String[] args) {
-        String []arr;
-        arr= new String[]{"ali", "veli", "deli"};
         int a[] = {1,2,3,4,5};
         int d[] = a;
         int sum = 0;
-
         for (int j = 0; j < 3; ++j)
-
             sum += (a[j] * d[j + 1]) + (a[j + 1] * d[j]);
-
         System.out.println(sum);
     }
 
