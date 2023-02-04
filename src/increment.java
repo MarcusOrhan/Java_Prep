2b9571e16d1259655aa0484ea8389f2bde5bd314
glnsb <marcusorhan@gmail.com>
2023-02-04 03:09:57 -0500

    increment

--- src/increment.java
+++ src/increment.java
@@ -1,15 +1,18 @@
 public class increment {
     public static void main(String[] args) {
-        int x = 1;
-        int y = 1;
+        int x = 4, y = 3, z = 2;
+        int result = x + y++ - --z * --x;
+        System.out.println(result);
 
-        if (x++<++y){
+       // int x = 1;
+     //   int y = 1;
+     //   if (x++<++y){
+      //      System.out.println("Hello");
+    //    }else{
+   //         System.out.println("Welcome");
+   //     }
 
-            System.out.println("Hello");
-        }else{
-            System.out.println("Welcome");
-        }
+
         System.out.println("Log "+x+" : "+ y);
     }
-
 }
