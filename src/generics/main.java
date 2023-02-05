5b29c0679268606f9131b9029d7406889fa8b522
glnsb <marcusorhan@gmail.com>
2023-02-05 02:26:22 -0500

    Generics MyList

--- src/generics/main.java
+++ src/generics/main.java
@@ -6,12 +6,15 @@ public class main {
     public static void main(String[] args) {
      // a class that can allow us   to work with different types class
        // tipguvenlicalismakicin generic clasla calisiriz yani Arraylist normalde object alir ; tipine<String> yada <Integer> dersek o zaman tip guvenli calismis oluruz
-        ArrayList<String> sehirler = new ArrayList<String>();
-        sehirler.add("Montreal");
-        sehirler.add("PhnomPenh");
-        sehirler.add("Ulanbator");
-        sehirler.add("Yozgat");
+//        ArrayList<String> sehirler = new ArrayList<String>();
+//        sehirler.add("Montreal");
+//        sehirler.add("PhnomPenh");
+//        sehirler.add("Ulanbator");
+//        sehirler.add("Yozgat");
 
+        MyList<Customer> sehirlerimiz = new MyList<Customer>();
+        sehirlerimiz.add(new Customer());
+        sehirlerimiz.add(new Customer());
 
     }
 }
