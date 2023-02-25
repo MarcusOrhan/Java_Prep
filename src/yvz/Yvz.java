437c13e9d411c05b4683ddcedcc36f63cf8cc672
glnsb <marcusorhan@gmail.com>
2023-02-25 00:04:43 -0500

    TAU Instant Credit Check

--- src/yvz/Yvz.java
+++ src/yvz/Yvz.java
@@ -3,5 +3,14 @@ package yvz;
 public class Yvz {
     public static void main(String[] args) {
 
+            int x=10;
+            int y=25;
+            int z=x+y;
+
+            System.out.println("Sum of x+y = " + z);
+        }
     }
-}
+
+
+
+
