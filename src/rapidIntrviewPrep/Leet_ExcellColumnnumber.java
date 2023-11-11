d3877aa66e0f94049644eb1385c887a81b6b442f
glnsb <marcusorhan@gmail.com>
2023-11-10 23:52:16 -0500

    leet code solutions

--- src/rapidIntrviewPrep/Leet_ExcellColumnnumber.java
+++ src/rapidIntrviewPrep/Leet_ExcellColumnnumber.java
@@ -3,7 +3,6 @@ package rapidIntrviewPrep;
 public class Leet_ExcellColumnnumber {
     public static void main(String[] args) {
         System.out.println(columnNumber("ABC"));
-
     }
 
     public static int columnNumber(String str){
@@ -12,8 +11,6 @@ public class Leet_ExcellColumnnumber {
         for (char each : charNumber ) {
             result = result*26 + (each-'A'+1);
         }
-
-
         return result;
     }
 }
