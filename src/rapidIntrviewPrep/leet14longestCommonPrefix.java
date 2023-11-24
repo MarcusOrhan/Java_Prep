aa4f4f646cc000073136d088debf8d2f4c83fe04
glnsb <marcusorhan@gmail.com>
2023-11-23 22:34:47 -0500

    leet 14

---
+++ src/rapidIntrviewPrep/leet14longestCommonPrefix.java
@@ -0,0 +1,23 @@
+package rapidIntrviewPrep;
+
+public class leet14longestCommonPrefix {
+    /*
+    Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+     */
+    public static void main(String[] args) {
+
+    }
+    public static String commonPrefix(String[]str){
+        String result = "";
+        for (int i = 0; i < str.length; i++) {
+
+            
+        }
+
+        return result;
+    }
+}
