bffb7e8bd76f35b9a337930f11a51dca4f229998
glnsb <marcusorhan@gmail.com>
2023-11-23 22:25:00 -0500

    leet 1108

--- src/rapidIntrviewPrep/leet1108defangIp.java
+++ src/rapidIntrviewPrep/leet1108defangIp.java
@@ -1,6 +1,12 @@
 package rapidIntrviewPrep;
 
+
 public class leet1108defangIp {
-
-
+    public static void main(String[]args){
+        System.out.println(ipDefang("1.200.255.255.0"));
+    }
+    public static String ipDefang (String str){
+        String result = str.replaceAll("\\.","[.]");
+        return result;
+    }
 }
